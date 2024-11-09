c288f4f5e18d1cb3d349b93ff0b99f12ac83c57a
juliajorkowitz <154336638+juliajorkowitz@users.noreply.github.com>
2024-11-09 12:02:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "math_quiz=math_quiz.math_quiz:main",  # `main` should be the entry function in `math_quiz.py`
+        ],
+    },
+)
