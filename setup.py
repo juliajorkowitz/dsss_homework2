6104e9b139fdcdfb9363e261b3fe3c732eb62d42
juliajorkowitz <154336638+juliajorkowitz@users.noreply.github.com>
2024-11-09 12:11:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "math_quiz=math_quiz.math_quiz:main",  # `main` should be the entry function in `math_quiz.py`
+            "math_quiz=math_quiz.math_quiz:math_quiz",  # `main` should be the entry function in `math_quiz.py`
         ],
     },
 )
